d2c1cc9a2017c6ce5bddd00498fdb84a3b08f544
Benny Elashkin <pavel.elashkin@gmail.com>
2022-04-04 20:56:45 +0300

    Добавьте файлы проекта.

---
+++ Lesson 4_Group_CO/CreationModel.cs
@@ -0,0 +1,82 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Group_CO
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+
+    public class CreationModel : IExternalCommand
+    {
+            public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            List<Level> listlevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            Level level1 = listlevel
+               .Where(x => x.Name.Equals("Уровень 1"))
+               .FirstOrDefault();
+            Level level2 = listlevel
+                .Where(x => x.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
+
+            GetWallDrawMethod(doc, level1, level2);
+
+
+            /*var res1 = new FilteredElementCollector(doc)
+                    .OfClass(typeof(WallType))
+                    .OfType<WallType>()
+                    .ToList();
+
+            var res2 = new FilteredElementCollector(doc)
+                    .OfClass(typeof(FamilyInstance))
+                    .OfCategory(BuiltInCategory.OST_Doors)
+                    .OfType<FamilyInstance>()
+                    .Where(f => f.Name.Equals("0915 x 2134 мм"))
+                    .ToList();
+
+            var res3 = new FilteredElementCollector(doc)
+                    .WhereElementIsNotElementType()
+                    .ToList();*/
+
+            return Result.Succeeded;
+        }
+
+        public static void GetWallDrawMethod(Document doc, Level level1, Level level2)
+        {
+            double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
+            double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
+            double dx = width / 2;
+            double dy = depth / 2;
+
+            List<XYZ> points = new List<XYZ>();
+            points.Add(new XYZ(-dx, -dy, 0));
+            points.Add(new XYZ(dx, -dy, 0));
+            points.Add(new XYZ(dx, dy, 0));
+            points.Add(new XYZ(-dx, dy, 0));
+            points.Add(new XYZ(-dx, -dy, 0));
+
+            Transaction transaction = new Transaction(doc, "Построение стены");
+            transaction.Start();
+
+            List<Wall> walls = new List<Wall>();
+            for (int i = 0; i < 4; i++)
+            {
+                Line line = Line.CreateBound(points[i], points[i + 1]);
+                Wall wall = Wall.Create(doc, line, level1.Id, false);
+                walls.Add(wall);
+                wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
+            }
+            transaction.Commit();
+        }
+    }
+}
